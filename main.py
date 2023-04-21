b9aa873d6afbc3e76bc4b20acbb64ac602cddab7
m4this <vidalmathis@gmail.com>
2023-04-21 23:22:46 +0200

    results to store data already calculated

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from model_EDP_ordre1 import ModelEDP, ModelEDP_a_cst
-from numpy import sin, pi
+import functions as f
 
-nsin = lambda X:  sin(X * 2 * pi)
+
+
 
 
 if __name__ == "__main__":
     #model1 = ModelEDP(sin, lambda t, x: 1)
     #model1.graph_3d()
 
-    model2 = ModelEDP_a_cst(nsin, 1)
+    model2 = ModelEDP_a_cst(f.nsin, 1)
     #model2.graph_3d()
-    #model2.graph_2d(nb_droites=8)
+    #model2.graph_2d(nb_droites=7)
     #model2.images_2d()
     model2.heat_map_2d()
 
--- model_EDP_ordre1.py
+++ model_EDP_ordre1.py
@@ -15,7 +15,7 @@ class ModelEDP:
                  u0: Callable,
                  a: Callable,
                  J: int = 10 ** 3,
-                 delta_t: float = 10 ** -4,
+                 delta_t: float = 10 ** -5,
                  N: int = 10 ** 3,
                  ):
         if delta_t > 0:
@@ -81,58 +81,56 @@ class ModelEDP_a_cst(ModelEDP):
                  a: float,
                  J: int = 10 ** 4,
                  delta_t: float = 10 ** -4,
-                 N: int = 3 * 10 ** 3):
+                 N: int = 3 * 10 ** 4):
 
         super().__init__(u0, lambda t, x: a, J, delta_t, N)
         self.a = a
 
 
-    def solve(self) -> (list[float], list[float], list[list[float]]):
-        dx = 1 / self.J
-        les_x = [(j + 1 / 2) * dx for j in range(self.J)]
 
-        dt = self.delta_t
-        les_t = [n * dt for n in range(self.N)]
-
-        les_u_0_j = [self.u0(x) for x in les_x]
-        les_u_n = [les_u_0_j]
-
-        for _ in trange(1, self.N):
-            u_prec = les_u_n[-1]  # u_n-1
-            u_n_0 = u_prec[0] - self.a * dt / dx * (u_prec[0] - u_prec[-1])
-            u_n = [u_n_0]
-
-            for j in range(1, self.J):
-                u_n_j = u_prec[j] - self.a * dt / dx * (u_prec[j] - u_prec[j-1])
-                u_n.append(u_n_j)
-
-            les_u_n.append(u_n)
-
-        return les_t, les_x, les_u_n
-
-    def solve_numpy(self):
+    def solve(self):
         dx = 1 / self.J
         dt = self.delta_t
 
         self.X = np.array([(j + 1 / 2) * dx for j in range(self.J)])
         self.T = np.array([n * dt for n in range(self.N)])
 
-        U = np.zeros((self.N, self.J), dtype="float128")
+        U = np.zeros((self.N, self.J))
 
         for j, x in enumerate(self.X):
             U[0, j] = self.u0(x)
 
         for n in trange(1, self.N, desc="Calcul des U_n_j"):
             for j in range(self.J):
-                U[n, j] = U[n-1, j] - self.a * dt / dx * (U[n-1, j] - U[n-1, j-1])
-            #print(max(U[n]))
-            m = max(U[n])
+                if (u_n_j := U[n-1, j] - self.a * dt / dx * (U[n-1, j] - U[n-1, j-1])) > 1e30:
+                    raise OverflowError(f"explosion de la fonction en t={self.T[n]}")
+                else:
+                    U[n, j] = u_n_j
 
         self.U = U
         return self.T, self.X, U
 
+
+    def results(self, regenerate:bool = False, save:bool = True):
+        func_name = self.u0.__name__
+        file_name = f"Data of {func_name} with param {self.a, self.J, self.delta_t, self.N}.npz"
+        path = "./data/" + file_name
+
+        if regenerate or not os.path.isfile(path):
+            T, X, U = self.solve()
+            if save:
+                np.savez_compressed(path, T=T, X=X, U=U)
+        else:
+            with np.load(path) as data:
+                T = data["T"]
+                X = data["X"]
+                U = data["U"]
+
+        return T, X, U
+
+
     def graph_3d(self):
-        les_t, les_x, U = self.solve_numpy()
+        les_t, les_x, U = self.results()
 
         X, T = np.meshgrid(les_x, les_t)
 
@@ -148,7 +146,7 @@ class ModelEDP_a_cst(ModelEDP):
 
     def graph_2d(self, nb_droites: int = 4):
         """Graphe de u(t,.) pour t fixé"""
-        les_t, les_x, U = self.solve_numpy()
+        les_t, les_x, U = self.results()
 
         ind_quantile = int(self.N / (nb_droites + 1))
         for k in range(nb_droites):
@@ -158,7 +156,7 @@ class ModelEDP_a_cst(ModelEDP):
         plt.show()
 
     def images_2d(self):
-        les_t, les_x, U = self.solve_numpy()
+        les_t, les_x, U = self.results()
 
         if not os.path.isdir("./images"):
             print("making dir")
@@ -174,7 +172,7 @@ class ModelEDP_a_cst(ModelEDP):
         images_to_gif(int(1/np.sqrt(self.delta_t)))
 
     def heat_map_2d(self):
-        les_t, les_x, U = self.solve_numpy()
+        les_t, les_x, U = self.results()
 
         plt.imshow(U, cmap='seismic')
         plt.colorbar()
